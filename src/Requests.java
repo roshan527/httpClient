84c15d7e8032059e18211a6cddfda7a601bdcead
roshan527 <findroshan527@gmail.com>
2019-09-02 22:05:27 +1000

    first commit

---
+++ src/Requests.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class Requests {
+
+public static void main(String args[]) throws Exception{
+
+	 //Create an HttpClient object
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    try{
+       //Create an HttpGet object
+       HttpGet httpget = new HttpGet("http://localhost:8080/UserManagement/rest/UserService/users");
+
+       //Execute the Get request
+       CloseableHttpResponse httpresponse = httpclient.execute(httpget);
+
+       try{
+          Scanner sc = new Scanner(httpresponse.getEntity().getContent());
+          while(sc.hasNext()) {
+             System.out.println(sc.nextLine());
+          }
+       }finally{
+          httpresponse.close();
+       }
+    }finally{
+       httpclient.close();
+    }
+}
+}
